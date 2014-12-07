14ed3e487179a2c7d4864ff3a560ed84077997c6
Viktor Lova <nsinreal@gmail.com>
2014-12-07 21:42:43 +0200

    removed unused functions

--- src/searchengine/helpers.clj
+++ src/searchengine/helpers.clj
@@ -13,69 +13,6 @@
   [seq elm]
   (some #(= elm %) seq))
 
-(defn- transform-node [node transformer]
-  (let [node (transformer node)]
-    (cond (map? node)
-          (zipmap
-           (map #(transform-node (key %) transformer) node)
-           (map #(transform-node (val %) transformer) node))
-
-          (vector? node)
-          (apply vector (map #(transform-node % transformer) node))
-
-          (list? node)
-          (apply list (map #(transform-node % transformer) node))
-
-          (seq? node)
-          (apply list (map #(transform-node % transformer) node))
-
-          :else
-          node)))
-
-(defn- get-ns [fn-sym]
-  (let [alias (symbol (string/replace (str fn-sym) #"/.*" ""))
-        ns-aliases (ns-aliases *ns*)]
-    (str
-     (if (contains? ns-aliases alias)
-       (get ns-aliases alias)
-       alias))))
-
-(defn- hook-arguments-one
-  [option code]
-  (letfn
-    [(transformer
-      [node]
-      (if (seq? node)
-        (let [method-call (first node)]
-          (if (or (in? (:for option) method-call)
-                  (in? (map get-ns (:for-ns option))
-                     (get-ns method-call)))
-            (concat (list method-call)
-                    (:prepend option)
-                    (rest node))
-            node))
-        node))]
-    (transform-node code transformer)))
-
-(defn- hook-arguments-with-options
-  [options code]
-  (if (seq? options)
-    (loop [option (first options)
-           options (next options)
-           code code]
-      (if (nil? option)
-        code
-        (recur (first options)
-               (next options)
-               (hook-arguments-one option code))))
-    (hook-arguments-one options code)))
-
-(defmacro hook-arguments
-  [options & code]
-  (hook-arguments-with-options
-   (concat deps/hook-options options)
-   `(do ~@code)))
-
 (defmacro with-resources
   [resources f]
   `(binding
