233e6c7e44dfe3c0e98c7a57363ab52f5e142a36
Viktor Lova <nsinreal@gmail.com>
2014-12-08 13:26:54 +0200

    updated outdated dependencies

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [org.clojure/clojurescript "0.0-2411"]
                  [om "0.8.0-beta2"]
                  [sablono "0.2.22"]
                  [fogus/ring-edn "0.2.0"]
                  [javax.servlet/servlet-api "2.5"]
-                 [ring/ring-core "1.3.1"]
+                 [ring/ring-core "1.3.2"]
                  [compojure "1.3.1"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.2"]
-                 [http-kit "2.1.16"]
-                 [clojurewerkz/elastisch "2.1.0-rc1"]
+                 [http-kit "2.1.19"]
+                 [clojurewerkz/elastisch "2.1.0"]
                  [clj-http "1.0.1"]
                  [hickory "0.5.4"]
                  [org.apache.commons/commons-lang3 "3.3.2"]
@@ -23,8 +23,8 @@
   :plugins [[hiccup-bridge "1.0.1"]
             [lein-swank "1.4.5"]
             [lein-cljsbuild "1.0.3"]]
-;;   :main searchengine.core
-;;   :aot :all
+  :main searchengine.core
+  :aot :all
   :cljsbuild
   {
    :builds [{:source-paths ["src-cljs"]
--- src/searchengine/site/core.clj
+++ src/searchengine/site/core.clj
@@ -1,13 +1,12 @@
-(
+(ns searchengine.site.core
   (:require
    [org.httpkit.server :as httpkit :refer :all]
    [ring.middleware.edn :refer [wrap-edn-params]]
    [compojure.handler :refer [site]]
    [searchengine.site.routes :refer [app-routes]]
    [searchengine.helpers :refer [to-middleware]]
-   (searchengine.core
-    [elastic :refer [wrap-with-elastic]]
-    [rabbitmq :refer [wrap-with-rabbitmq]])))
+   [searchengine.core.elastic :refer [wrap-with-elastic]]
+   [searchengine.core.rabbitmq :refer [wrap-with-rabbitmq]]))
 
 (defn run [port]
   (run-server
